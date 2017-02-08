a12761ce46485daf99ba9ef8f59f2862daaea81c
CameronCurrie <admin@cameroncurrie.co.uk>
2017-02-08 20:36:44 +0000

    Update MDCore.py

--- MDCore.py
+++ MDCore.py
@@ -2,21 +2,22 @@ import MDUtilities as MDU
 import numpy as np
 import math
 import VectorMethods as VM
-import VerletVelocity as Vv
-from Particle3D import Particle3D as Particle3D
+import VerletVelocity as Ver
+from Particle3D import Particle3D as p
 
 
-if len(sys.argv)!=2:
-	print ("Wrong number of arguments.")
-	print ("Usage: " + sys.argv[0] + " <output file>")
+if len(sys.argv)!=3:
+	print "Wrong number of arguments."
+	print "Usage: " + sys.argv[0] + " <output file>"
 	quit()
 else:
 	outfileName = sys.argv[1]
+    conditions = sys.argv[2]
 #Open output file for writing data to
 outfile = open(outfileName,"w")
 
-
-file_handle = open("inFile.txt","r")
+file_handle = open(conditions,"r")
 
 #set up the intial positions of all particles
-InitialPosition = MDU.setInitialPositions(rho, particles)
+InitialPositions = MDU.setInitialPositions(rho, particles)
+InitialVelocities = MDU.setInitialVelocities(temp)
